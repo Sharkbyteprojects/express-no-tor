954b9c2baa38ce3be657e6e3b962e7a7fdc3b8b5
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-03-08 19:06:16 +0100

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# NOTOR
+
+Example:
+`
+const app=require("express")();
+const notor = require("notor");
+app.use(notor("<h1>Tor not Allowed</h1>"));
+app.get("/",(req,res)=>{
+res.json({"You are not Using tor:)"});
+});
+app.listen(8080,()=>{console.log("Listen");});
+`
---
+++ notor.js
@@ -0,0 +1,19 @@
+const dns=require("dns");
+const toronion=["tor", "onion", "exit", "anonymizer","relay", "ip-", "today", "accessnow.org"];
+module.exports=(warnmessage)=>{
+	return (req,res,next)=>{
+		new Promise((res,rej)=>{
+dns.reverse(req.connection.remoteAddress,(err,hostnames)=>{
+	let ok=true;
+				if(hostnames&&hostnames!==undefined&&hostnames!==[]&&hostnames[0]!=null){
+					
+					hostnames.forEach(sz=>{
+					if(sz.includes(toronion[0])||sz.includes(toronion[1])||sz.includes(toronion[2])||sz.includes(toronion[3])||sz.includes(toronion[4])||sz.includes(toronion[5])||sz.includes(toronion[6])||sz.includes(toronion[7])){
+						rej(true);
+						ok=false;
+					}});
+					
+				}if(ok){res(ok);}
+			});}).then((ok)=>{next();},err=>{if(err){res.send(warnmessage);}});
+}
+}
