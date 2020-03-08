636367d6986d91a6af19c0fc89ebe14f1085b6da
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-03-08 19:19:54 +0100

    Update notor.js

--- notor.js
+++ notor.js
@@ -6,13 +6,15 @@ module.exports=(warnmessage)=>{
 dns.reverse(req.connection.remoteAddress,(err,hostnames)=>{
 	let ok=true;
 				if(hostnames&&hostnames!==undefined&&hostnames!==[]&&hostnames[0]!=null){
-					
+					ok=false;
+					let detect=false;
 					hostnames.forEach(sz=>{
 					if(sz.includes(toronion[0])||sz.includes(toronion[1])||sz.includes(toronion[2])||sz.includes(toronion[3])||sz.includes(toronion[4])||sz.includes(toronion[5])||sz.includes(toronion[6])||sz.includes(toronion[7])){
 						rej(true);
 						ok=false;
+						detect=true;
 						res.send(warnmessage);
-					}});
+					}});if(!detect){ok=true}
 					
 				}if(ok){res(ok);}
 			});}).then((ok)=>{next();},err=>{if(err){console.log("Tor");}});
